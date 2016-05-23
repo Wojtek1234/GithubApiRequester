b7ef4497bfff22d516b145fec36f4c042726999f
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2016-05-23 11:14:24 +0200

    change api call to return good classes

--- app/src/main/java/com/wojciechmaciejewski/githubapirequester/network/GithubApi.kt
+++ app/src/main/java/com/wojciechmaciejewski/githubapirequester/network/GithubApi.kt
@@ -2,6 +2,8 @@
 
 package com.wojciechmaciejewski.githubapirequester.network
 
+import com.wojciechmaciejewski.githubapirequester.model.network.GithubRepo
+import com.wojciechmaciejewski.githubapirequester.model.network.GithubUser
 import retrofit2.http.GET
 import retrofit2.http.Query
 import rx.Observable
@@ -15,8 +17,9 @@ interface GithubApi{
 
 
     @GET("search/users")
-    fun askForUsers(@Query("q") serchFor:String,@Query("page") pageNumber: Int?):Observable<Unit>
+    fun askForUsers(@Query("q") serchFor:String,@Query("page") pageNumber: Int?):Observable<GithubUser>
 
     @GET("search/repositories")
-    fun askForRepos(@Query("q") serchFor:String,@Query("page") pageNumber: Int?):Observable<Unit>
+    fun askForRepos(@Query("q") serchFor:String,@Query("page") pageNumber: Int?):Observable<GithubRepo>
+
 }
