e695ba49c50248628cc53b76fb260a6fd4bc17bb
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2016-05-27 16:29:20 +0200

    update url to home url

--- app/src/main/java/com/wojciechmaciejewski/githubapirequester/model/network/GithubResponses.kt
+++ app/src/main/java/com/wojciechmaciejewski/githubapirequester/model/network/GithubResponses.kt
@@ -6,7 +6,8 @@ import com.google.gson.annotations.SerializedName
 /**
  *
  */
-data class GithubUser(val id:Int, val login:String,@SerializedName("url") val homepage:String)
-data class GithubRepo(val id:Int, val name:String,@SerializedName("url") val homepage:String, val owner: GithubUser)
+data class GithubUser(val id: Int, val login: String, @SerializedName("html_url") val homepage: String)
+
+data class GithubRepo(val id: Int, val name: String, @SerializedName("html_url") val homepage: String, val owner: GithubUser)
 data class GithubUserResponse(val items: List<GithubUser>, @SerializedName("total_count") val totalCount: Int)
 data class GithubRepoResponse(val items: List<GithubRepo>, @SerializedName("total_count") val totalCount: Int)
