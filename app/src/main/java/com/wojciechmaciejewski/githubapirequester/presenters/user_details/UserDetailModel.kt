17fbe5cb4a0984c2645778f2a09dad04b237e931
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2016-05-28 19:53:35 +0200

    add implementation for UserDetailModel

--- app/src/main/java/com/wojciechmaciejewski/githubapirequester/presenters/user_details/UserDetailModel.kt
+++ app/src/main/java/com/wojciechmaciejewski/githubapirequester/presenters/user_details/UserDetailModel.kt
@@ -1,9 +1,6 @@
 package com.wojciechmaciejewski.githubapirequester.presenters.user_details
 
-import com.wojciechmaciejewski.githubapirequester.model.network.GithubUser
-import com.wojciechmaciejewski.githubapirequester.model.network.GithubUserDetail
 import com.wojciechmaciejewski.githubapirequester.network.GithubUserDetailApi
-import rx.Observable
 import javax.inject.Inject
 
 /**
@@ -11,18 +8,14 @@ import javax.inject.Inject
  */
 
 class UserDetailModel : UserDetail.Model {
-    override fun loadDataForUser(userName: String): Observable<GithubUserDetail> {
-        throw UnsupportedOperationException()
-    }
-
-    override fun loadUserFollowers(userName: String): Observable<List<GithubUser>> {
-        throw UnsupportedOperationException()
-    }
-
     val gihubApi: GithubUserDetailApi
 
     @Inject
     constructor(githubUserDetailApi: GithubUserDetailApi) {
         this.gihubApi = githubUserDetailApi;
     }
+
+    override fun loadDataForUser(userName: String) = gihubApi.getUserDetails(userName)
+
+    override fun loadUserFollowers(userName: String) = gihubApi.getUserFollowers(userName)
 }
